2317b53e6e1d4fb1a2f2446edb5b382a424c2b78
Sebastian Zan <sz@subey.net>
2015-09-05 21:53:45 +0200

    Add gradle and update readme

--- src/main/java/com/bethecoder/App.java
+++ src/main/java/com/bethecoder/App.java
@@ -1,4 +1,6 @@
-package com.bethecoder;
+package com.bethecoder.ascii_table;
+
+import java.lang.System;
 
 /**
  * Hello world!
@@ -9,5 +11,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
     }
 }
